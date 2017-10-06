5caad6703f351bc8b409b9d54a95e7d96f904826
petersee <petersee98@gmail.com>
2017-10-06 13:17:11 +0800

    jtyj

---
+++ ACI_FrontEndWeb_Development/AllPages/E.Events/C_EventDetails.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DataAccessLayer;
+
+
+
+namespace ACI_FrontEndWeb_Development.AllPages.E.Events
+{
+    public partial class C_EventDetails : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
